d748089eb00443e75541a0b3e373de0139894639
SMCorey <sloanc97@gmail.com>
2025-01-21 11:29:18 -0400

    discussion and comment models created.

---
+++ Models/Discussion.cs
@@ -0,0 +1,14 @@
+﻿namespace WarhammerForum.Models
+{
+    public class Discussion
+    {
+        public int DiscussionId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string ImageFilename { get; set; } = string.Empty ;
+        public DateTime CreateDate { get; set; }
+
+        // Navigation
+        public List<Comment>? Comments { get; set; }
+    }
+}
