d27c6c6344e2be1553ba40ab9bdafb2c80ec9c33
SMCorey <sloanc97@gmail.com>
2025-02-02 20:04:59 -0400

    scaffolding of comment and discussion models (WiP)

---
+++ Data/WarhammerForumContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WarhammerForum.Models;
+
+namespace WarhammerForum.Data
+{
+    public class WarhammerForumContext : DbContext
+    {
+        public WarhammerForumContext (DbContextOptions<WarhammerForumContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WarhammerForum.Models.Discussion> Discussion { get; set; } = default!;
+        public DbSet<WarhammerForum.Models.Comment> Comment { get; set; } = default!;
+    }
+}
